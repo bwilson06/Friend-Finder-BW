7480db50c0f28b16614d551dd0b01c6b887568ef
Bradley Wilson <bluedirtbike6@gmail.com>
2020-06-01 14:35:46 -0500

    add apiRoutes logic.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,29 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res){
-        friendsArray.push(req.body)
+        var newScores = req.body.scores
+        var bffName = '';
+        var bffPic = '';
+        var totalDiff = 50;
+
+        console.log(newScores)
+
+        for (var i = 0; i < friendsArray.length; i ++){
+            var difference = 0;
+
+            for (var j = 0; j < newScores.length; j++){
+                difference += (Math.abs(parseInt(friendsArray[i].scores[j])) - parseInt((newScores[j])));
+            }
+
+            if (difference < totalDiff){
+                bffName = friendsArray[i].name;
+                bffPic = friendsArray[i].photo;
+                totalDiff = difference;
+            }
+        }
+        friendsArray.push(req.body);
+        res.json({status: 'OK', name: bffName, photo: bffPic})
+        
     });
+    
 };
